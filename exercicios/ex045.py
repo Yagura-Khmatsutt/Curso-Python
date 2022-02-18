21afadbed1b75323c56feda103b338c54d7e7a0d
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-18 05:38:26 -0300

    Update ex045.py

--- exercicios/ex045.py
+++ exercicios/ex045.py
@@ -14,3 +14,30 @@ print('O jogador jogou {}'.format(itens[escolha]))
 print('O computador jogou {}'.format(itens[computador]))
 print('-=' * 20)
 
+if computador == 0:
+    if escolha == 0:
+        print('EMPATE')
+    elif escolha == 1:
+        print('VOCE PERDEU!')
+    elif escolha == 2:
+        print(' VOCE GANHOU')
+    else:
+        print('jogada invalida')
+elif computador == 1:
+    if escolha == 0:
+        print('VOCE GANHOU')
+    elif escolha == 1:
+        print('EMPATE')
+    elif escolha == 2:
+        print('VOCE PERDEU')
+    else:
+        print('jogada invalida')
+elif computador == 2:
+    if escolha == 1:
+        print('VOCE PERDEU')
+    elif escolha == 1:
+        print('VOCE GANHOU')
+    elif escolha == 2:
+        print('EMPATE')
+    else:
+        print('jogada invalida')
