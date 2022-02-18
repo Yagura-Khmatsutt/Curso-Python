20176691b7c02540997596d3108c08c0673f11eb
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-18 05:25:07 -0300

    Update ex045.py

--- exercicios/ex045.py
+++ exercicios/ex045.py
@@ -1,1 +1,13 @@
+from random import randint
 print('{:=^50}'.format(' JO KEN PO '))
+
+itens = ('Papel', 'Pedra', 'Tesoura')
+computador = randint(0, 2)
+print("""Escolha suas opções
+[0] PAPEL
+[1] PEDRA
+[2]TESOURA""")
+escolha = int(input('Qual a sua escolha?'))
+
+print('O jogador jogou {}'.format(escolha))
+print('O computador jogou {}'.format(computador))
