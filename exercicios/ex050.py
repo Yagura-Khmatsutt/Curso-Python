d4dbdbd416928001edfd8256d9f2b87b674a0f1b
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-22 20:28:26 -0300

    Update ex050.py

--- exercicios/ex050.py
+++ exercicios/ex050.py
@@ -1,1 +1,9 @@
 print('Somando pares')
+soma = 0
+cont = 0
+for contagem in range(1,7):
+    num = int(input('Dígite o {} número:'.format(contagem)))
+    if num % 2 == 0:
+        soma += num
+        cont += 1
+print('Os valores são {} e o total é {}'.format(cont, soma))
