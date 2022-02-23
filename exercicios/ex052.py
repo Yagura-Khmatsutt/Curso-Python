abb22caa7ab39451f0602af705715a261a265640
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-22 21:05:31 -0300

    Update ex052.py

--- exercicios/ex052.py
+++ exercicios/ex052.py
@@ -4,9 +4,14 @@ numer = int(input('Dígite um número:'))
 tot = 0
 for cont in range(1, numer+ 1):
     if numer % cont == 0:
-        print('\033[34m', end=' ')
+        print('\033[33m', end=' ')
         tot += 1
     else:
-        print('\033[35m', end='')
+        print('\033[34m', end=' ')
+    print('{}'.format(cont), end=' ')
 
-print('O número {} foi divisivel por {}.'.format(numer))
+print('\nO número {} foi divisivel por {} vezes.'.format(numer, tot))
+if tot == 2:
+    print('\033[032mO número é PRIMO')
+else:
+    print('\033[031mEle NÃO É PRIMO')
