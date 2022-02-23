69ea675e03b46547377f74dd6ceac82e1324fbd5
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-23 20:52:54 -0300

    Update ex057.py

--- exercicios/ex057.py
+++ exercicios/ex057.py
@@ -2,4 +2,5 @@ print('Validação de dados')
 
 sexo = str(input('Qual o sexo [M/F]:')).strip().upper()[0]
 while sexo not in 'MmFf':
-    print('ERROR [Dado invalido!]')
+    sexo = str(input('ERROR [Dado invalido!], Por favor degite novamente o sexo [M/F]')).strip().upper()[0]
+print('Sexo {} registrado comsucesso!'.format(sexo))
