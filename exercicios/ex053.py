cc54e1e32c1f1148fa3843274680ae781ce68948
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-23 04:58:40 -0300

    pronto

--- exercicios/ex053.py
+++ exercicios/ex053.py
@@ -1,10 +1,15 @@
 print('PALINDROMO!')
 
 frase = str(input('Escreva um fraze e teste se é um Palindromo:')).strip().upper()
-palavra = frase.strip()
-junto = ''.join(palavra)
-print('apenas um teste {}'.format(junto))
+palavras = frase.split()
+junto = ''.join(palavras)
 inverso = ''
 for letra in range(len(junto) -1, -1, -1):
     inverso += junto[letra]
-    print(inverso)
+if inverso == junto:
+    print('A frase {} é um Palindrmo'.format(junto))
+else:
+    print('A frase não é um palindromo!')
+print(junto, inverso, end='')
+
+
