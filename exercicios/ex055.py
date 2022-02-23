86d3494fd1a72e89fe17953b1aa599354d3e7f93
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-23 05:31:21 -0300

    ex056

--- exercicios/ex055.py
+++ exercicios/ex055.py
@@ -1,1 +1,15 @@
 print('O maior e menor na sequencia')
+maiorp = 0
+menorp = 0
+for p in range(1,6):
+    peso = float(input('Peso da {}ª pessoa:'.format(p)))
+    if p == 1:
+        maiorp = peso
+        menorp = peso
+    else:
+        if peso > maiorp:
+            maiorp = peso
+        if peso < menorp:
+            menorp = peso
+print('O maior peso é {}Kg.'.format(maiorp))
+print('O menor peso e {}Kg'.format(menorp))
