79bd0d61cd9bd979db6fcda0161e3b9d83e8c576
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-24 19:42:50 -0300

    Update ex065.py

--- exercicios/ex065.py
+++ exercicios/ex065.py
@@ -1,7 +1,10 @@
 print('Maior e menor valores!')
 rsp = 'S'
+soma = quantia = media = 0
 while rsp in 'Ss':
     n = int(input('Dígite um valor:'))
-
-    res = str(input("Deseja continuar [S/N]: ")).strip().upper()[0]
-print('Acabou por iquanto')
+    soma += n
+    quantia += 1
+    rsp = str(input("Deseja continuar [S/N]: ")).strip().upper()[0]
+media = soma / quantia
+print('Vocẽ digitou {} números e a média foi {}'.format(quantia, media))
