50494f10736123648818207cfd69e9b8655b6c29
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-23 21:08:04 -0300

    Update ex058.py

--- exercicios/ex058.py
+++ exercicios/ex058.py
@@ -6,7 +6,16 @@ sleep(2)
 print('Acabei de pensar, é um número entre 0 e 10!')
 print('Voce consegue acertar?')
 acertou = False
-
+palpite = 0
 while not acertou:
     num = int(input('Qual é o seu palpite?'))
-    
+    palpite += 1
+    if num == computer:
+        acertou = True
+    else:
+        if num < computer:
+            print('Aumente um pouco!')
+        if num > computer:
+            print('Diminua um pouco!')
+print('Acertou! e vc gastou {} palpites'.format(palpite))
+
