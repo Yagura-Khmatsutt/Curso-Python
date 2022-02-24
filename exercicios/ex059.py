8e0272b3942866c592b02e10a448397193fcfcd4
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-24 04:49:32 -0300

    Update ex059.py

--- exercicios/ex059.py
+++ exercicios/ex059.py
@@ -1,3 +1,10 @@
 print('Menu de opções')
 
 num1 = int(input('Dígite um valor inteiro:'))
+num2 = int(input('Digite um valor inteiro:'))
+
+print('''[1]somar
+[2]multiplicar
+[3]maior
+[4]novos numerors
+[5]sair''')
