f10e4833f33de1d89eb6e272b7c1b90c964c7d69
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-24 05:35:34 -0300

    ex061

--- exercicios/ex060.py
+++ exercicios/ex060.py
@@ -1,7 +1,10 @@
 print('fatorial')
 num = int(input('Dígite um número para ver seu fatorial:'))
 c = num
+f = 1
 while c > 0:
+    print('{} '.format(c), end='')
+    print(' x ' if c > 1 else '= ', end='')
+    f *= c
     c -= 1
-    print('{} x '.format(c), end='')
-    print(' x ' if c > 1 else '=', end='')
+print(f)
