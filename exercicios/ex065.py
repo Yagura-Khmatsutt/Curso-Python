332b2b8f825acd8ed14774d26fe4acfdf7377ed5
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-24 19:35:30 -0300

    Update ex065.py

---
+++ exercicios/ex065.py
@@ -0,0 +1,7 @@
+print('Maior e menor valores!')
+rsp = 'S'
+while rsp in 'Ss':
+    n = int(input('Dígite um valor:'))
+
+    res = str(input("Deseja continuar [S/N]: ")).strip().upper()[0]
+print('Acabou por iquanto')
