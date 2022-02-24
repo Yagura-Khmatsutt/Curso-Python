1c9e0f6556b6f43d46def59a8f938f4bd9e140b4
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-24 05:02:43 -0300

    Update ex059.py

--- exercicios/ex059.py
+++ exercicios/ex059.py
@@ -2,9 +2,29 @@ print('Menu de opções')
 
 num1 = int(input('Dígite um valor inteiro:'))
 num2 = int(input('Digite um valor inteiro:'))
+opçao = 0
+while opçao != 5:
+    print('''   
+    [1]somar
+    [2]multiplicar
+    [3]maior
+    [4]novos numerors
+    [5]sair''')
+    opçao = int(input('Qual a opção:'))
+    if opçao == 1:
+       soma = num1 + num2
+       print('o valor é {}'.format(soma))
 
-print('''[1]somar
-[2]multiplicar
-[3]maior
-[4]novos numerors
-[5]sair''')
+    elif opçao == 2:
+        mult = num1 * num2
+        print('O valor é {}'.format(mult))
+    elif opcao == 3:
+
+    elif opcao == 4:
+
+    elif opcao == 5:
+
+    else:
+        print('Dígite outro número')
+
+    print('Fim do programa')
