9b7a248f3aa79a87c83aea711d5d2cde9b145c79
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-25 20:45:04 -0300

    Update ex070.py

--- exercicios/ex070.py
+++ exercicios/ex070.py
@@ -6,13 +6,23 @@ A) qual é o total gasto na compra.
 B) quantos produtos custam mais de R$1000.
 
 C) qual é o nome do produto mais barato.'''
+cont = valor = total = menor = 0
 
 while True:
-
     produto = str(input('Nome do produto: '))
     preço = float(input('Preço do produto: '))
     resp = ' '
+    cont += 1
+    total += preço
+    if preço >= 1000:
+        valor += 1
+    if cont == 1:
+        menor = preço
+    else:
+        if preço 
     while resp not in 'SN':
         resp = str(input('Deseja continuar [S/N]: ')).strip().upper()[0]
     if resp == 'N':
         break
+print(f'Você comprou {cont} produtos e custara no total de R$ {total}.')
+print(f'Vocẽ comprou {valor} produtos assima de R$1000.00')
