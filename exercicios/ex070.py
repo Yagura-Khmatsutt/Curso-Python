3ca0222919d1dfb64a58c3839a1183fa9ac6542e
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-25 19:59:46 -0300

    Update ex070.py

--- exercicios/ex070.py
+++ exercicios/ex070.py
@@ -1,1 +1,8 @@
 print('Estatisticas de Produtos')
+while True:
+    produto = str(input('Nome do produto: '))
+    preço = float(input('Preço do produto: '))
+    resp = str(input('Deseja continuar [S/N]: ')).strip().upper()[0]
+    if resp == 'N':
+        break
+    
