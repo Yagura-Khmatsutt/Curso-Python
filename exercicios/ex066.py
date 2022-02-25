9e38f9701188b4a4fe9f3075fa8766326c86cb0d
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-25 05:02:13 -0300

    Update ex066.py

--- exercicios/ex066.py
+++ exercicios/ex066.py
@@ -1,6 +1,8 @@
-n = soma = cont = 0
-while n != 999:
+soma = cont = 0
+while True:
     n = int(input('Dígite um número: [999 para parar]:'))
+    if n == 999:
+        break
     soma += n
     cont += 1
 print(f'Você dígitou {cont} númros e a soma é {soma}')
