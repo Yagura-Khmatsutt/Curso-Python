12653e30c5df957ca13d64de7bf9c112e108510f
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-25 21:07:43 -0300

    Update ex071.py

---
+++ exercicios/ex071.py
@@ -0,0 +1,26 @@
+print('Caixa eletrônico')
+print('=' * 30)
+print('{:^30}'.format('Banco Do Python'))
+print('=' * 30)
+
+valor = int(input('Quanto deseja sacar: R$'))
+total = valor
+ced = 50
+totced = 0
+
+while True:
+   if totced >= ced:
+       total -= ced
+       totced += 1
+   else:
+       if totced > 0:
+         print(f'Total de {totced} cédulas {ced}')
+       elif ced == 50:
+           ced = 20
+       elif ced == 20:
+           ced = 10
+       elif ced == 10:
+           ced = 1
+           totced = 0
+
+
