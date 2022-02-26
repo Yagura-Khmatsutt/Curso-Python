a63df16f7ea378d0bbf548962dd02bdb280f2f23
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-25 21:17:17 -0300

    Update ex071.py

--- exercicios/ex071.py
+++ exercicios/ex071.py
@@ -9,18 +9,19 @@ ced = 50
 totced = 0
 
 while True:
-   if totced >= ced:
-       total -= ced
-       totced += 1
-   else:
-       if totced > 0:
-         print(f'Total de {totced} cédulas {ced}')
-       elif ced == 50:
-           ced = 20
-       elif ced == 20:
-           ced = 10
-       elif ced == 10:
-           ced = 1
-           totced = 0
-
-
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+print('Volte sempre,Bom Dia!')
