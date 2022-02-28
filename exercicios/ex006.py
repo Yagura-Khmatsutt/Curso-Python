94f1520e7404af52afe4ad43a3207e62222f4320
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-28 14:35:42 -0300

    Update ex006.py

---
+++ exercicios/ex006.py
@@ -0,0 +1,13 @@
+from math import sqrt
+'''
+Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+'''
+
+print('Olá')
+num = float(input('Dígite um número: '))
+
+dobro = num * 2
+triplo = num * 3
+raiz = sqrt(num)
+print(f'O número {num} tem seu dobro {dobro} e triplo {triplo} e a raiz {raiz}')
+print('FIM!')
