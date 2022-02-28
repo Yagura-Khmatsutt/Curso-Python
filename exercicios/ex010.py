4db694c3923e99837bb77640cbc1f258df6fb28a
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-28 15:08:29 -0300

    Update ex010.py

---
+++ exercicios/ex010.py
@@ -0,0 +1,10 @@
+'''
+Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+'''
+
+print('Olá')
+reais = float(input('Quantidade em reais: R$'))
+dolare = 5.16
+dolar = reais / dolare
+print(f'você tem {reais}R$ e pode comprar {dolar:.2f}$ dolares')
+print('FIM!')
