2ab21ea18a132ee09640fce87d1a4222f361769a
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-28 13:45:00 -0300

    Update ex003.py

---
+++ exercicios/ex003.py
@@ -0,0 +1,9 @@
+'''
+Crie um programa que leia dois números e mostre a soma entre eles.
+'''
+print('Olá,')
+num1 = float(input('Dígite um número:'))
+num2 = float(input('Dígite outro número:'))
+print(f'{num1} + {num2} = ', num1 + num2)
+
+print('FIM!')
