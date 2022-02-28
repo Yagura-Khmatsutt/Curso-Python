f5a6c6c251d809f91f2635babda3d348575e7420
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-28 14:30:57 -0300

    Create ex005.py

---
+++ exercicios/ex005.py
@@ -0,0 +1,10 @@
+'''
+Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+'''
+print('Olá')
+
+num = float(input('Dígite um número para ver seu sucessor e antecessor: '))
+antecessor = num - 1
+sucessor = num + 1
+print(f'O número {num} tem como antecessor {antecessor} e {sucessor} como sucessor')
+print('FIM')
