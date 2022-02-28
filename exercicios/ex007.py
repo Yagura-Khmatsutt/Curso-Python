85463d278c32bdd487843f212b8b62e7b85b8750
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-28 14:38:33 -0300

    Create ex007.py

---
+++ exercicios/ex007.py
@@ -0,0 +1,8 @@
+'''Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.'''
+print('Olá')
+nota1 = float(input('Dígite a primeira nota:'))
+nota2 = float(input('Dígite a segunda nota:'))
+
+media = (nota2 + nota1) /2
+print(f'a media das notas é {media}.')
+print('FIM')
