897c2be82a9d0aff3e07bb486828c3213a07af96
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-28 20:51:26 -0300

    Update ex021.py

--- exercicios/ex021.py
+++ exercicios/ex021.py
@@ -1,5 +1,11 @@
 '''
 Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 '''
-import playsound
+import pygame
+
+pygame.init()
+
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play(loops=0, start=0.0)
+pygame.event.wait()
 print('Fim')
