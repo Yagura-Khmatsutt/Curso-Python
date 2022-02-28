959a5ba65680e6dca8345223283f04696494449e
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-28 15:01:44 -0300

    Create ex009.py

---
+++ exercicios/ex009.py
@@ -0,0 +1,19 @@
+'''
+Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+'''
+print('Olá')
+num = int(input('Dígite um número inteiro para calcular a tabuada: '))
+print('-' * 30)
+print(f'{num} X 00 = {num * 0} ')
+print(f'{num} X 01 = {num * 1} ')
+print(f'{num} X 02 = {num * 2} ')
+print(f'{num} X 03 = {num * 3} ')
+print(f'{num} X 04 = {num * 4} ')
+print(f'{num} X 05 = {num * 5} ')
+print(f'{num} X 06 = {num * 6} ')
+print(f'{num} X 07 = {num * 7} ')
+print(f'{num} X 08 = {num * 8} ')
+print(f'{num} X 09 = {num * 9} ')
+print(f'{num} X 10 = {num * 10}')
+print('-' * 30)
+print('FIM')
