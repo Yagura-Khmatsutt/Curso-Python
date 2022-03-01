3d78431472303a9b427868a8d53572ebdf2c5d7f
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-01 08:32:05 -0300

    Create ex030.py

---
+++ exercicios/ex030.py
@@ -0,0 +1,10 @@
+'''
+Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+'''
+print('Olá')
+num = int(input('Dígite um número:'))
+if num % 2 == 0:
+    print(f'O número {num} é par.')
+else:
+    print(f'O número {num} é impar')
+print('FIM')
