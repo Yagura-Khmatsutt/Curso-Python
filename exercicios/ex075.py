31a4fed53da3851a8a6a147f7142e8f6935d0213
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-03 06:04:24 -0300

    Update ex075.py

--- exercicios/ex075.py
+++ exercicios/ex075.py
@@ -1,1 +1,12 @@
-''''''
+'''
+Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
+
+A) Quantas vezes apareceu o valor 9.
+
+B) Em que posição foi digitado o primeiro valor 3.
+
+C) Quais foram os números pares.
+
+Aula Anterior
+
+'''
