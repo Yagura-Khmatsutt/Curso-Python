c71019a41f75251d6e0d7f472bec045fc878f5bd
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-04 20:48:07 -0300

    Update ex077.py

--- exercicios/ex077.py
+++ exercicios/ex077.py
@@ -4,6 +4,10 @@ Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 '''
 
 print('Olá')
-palavras = ('Melancia', 'Pizza', 'Algodao', 'Joaquim', 'pao', 'carro', 'mula', 'escola', 'cachaca')
-print(palavras)
-print('FIM')
+palavras = ('Melancia', 'Pizza', 'Algodao', 'Joaquim', 'pao', 'carro', 'mula', 'escola', 'cachaca', 'desenvolvimento')
+for p in palavras:
+    print(f'\nNa palavra {p.upper()} temos', end=' ')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
+print('\nFIM')
