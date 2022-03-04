452990ab0e27d9b9833d5c651ffedfd0ec2ace5a
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-04 20:40:01 -0300

    Update ex077.py

--- exercicios/ex077.py
+++ exercicios/ex077.py
@@ -1,3 +1,8 @@
 '''
-
+Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
+Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 '''
+
+print('Olá')
+
+print('FIM')
