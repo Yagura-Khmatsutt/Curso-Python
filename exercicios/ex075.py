95bb7f5b0b5d5958593cfe2bd185924d64fb0fd5
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-03 21:41:12 -0300

    Update ex075.py

--- exercicios/ex075.py
+++ exercicios/ex075.py
@@ -6,7 +6,18 @@ A) Quantas vezes apareceu o valor 9.
 B) Em que posição foi digitado o primeiro valor 3.
 
 C) Quais foram os números pares.
-
-Aula Anterior
-
 '''
+print('Olá')
+n = (int(input('Dígite um número:')), int(input('Dígite um número:')), int(input('Dígite um número:')),
+     int(input('Dígite um número:')))
+print(n)
+print(f'O o número 9 apareceu {n.count(9)} vezes!')
+if 3 in n:
+    print(f'O número o primeiro número 3 apareceu na posição {n.index(3)+1}')
+else:
+    print('O valor 3 não apareceu')
+print('Os vaores pares aprecem em:', end='')
+for num in n:
+    if num % 2 == 0:
+        print(num,)
+print('FIM')
