12f65310caf6957435bbdaa9a8293ee7823b232d
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-06 19:55:44 -0300

    Update ex081.py

--- exercicios/ex081.py
+++ exercicios/ex081.py
@@ -7,18 +7,24 @@ C) Se o valor 5 foi digitado e está ou não na lista.
 '''
 from time import sleep
 valor = list()
-for i in range(0, 5):
+while True:
     n = int(input('Dígite um valor: '))
     if n not in valor:
         valor.append(n)
         print('Processando...')
-        sleep(1)
+        #sleep(1)
         print('Valor guardado com sucesso!')
     else:
         print('Processando...')
-        sleep(1)
+        #sleep(1)
         print('Valor duplicado![NÃO SALVO!]')
     r = str(input('Deseja continuar? [S/N]')).upper().strip()
     if r == 'N':
         break
-print(valor)
+if 5 in valor:
+    print('O valor 5 foi digitado')
+else:
+    print('O valor 5 não está na lista!')
+print(f'Foram dígitados {len(valor)} elementos')
+valor.sort(reverse=True)
+print(f'Os valores em ordem decrescente são {valor}')
