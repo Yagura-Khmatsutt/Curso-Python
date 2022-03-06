8f80726cbe38ec95af948a9997a08dd778182fc1
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-06 11:41:03 -0300

    Update ex078.py

---
+++ exercicios/ex078.py
@@ -0,0 +1,8 @@
+'''
+Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
+No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
+'''
+
+valores = list()
+for c in range(0,5):
+    valor = int(input(f'Dígite um valor para posição {c} :'))
