3da2a55841964b47de71df87e981179d4f1eb448
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-06 19:14:15 -0300

    Update ex079.py

--- exercicios/ex079.py
+++ exercicios/ex079.py
@@ -3,4 +3,21 @@ Crie um programa onde o usuário possa digitar vários valores numéricos e cada
 Caso o número já exista lá dentro, ele não será adicionado.
 No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
 '''
+from time import sleep
+valor = list()
 
+while True:
+    n = int(input('Dígite um valor: '))
+    if n not in valor:
+        valor.append(n)
+        print('Processando...')
+        sleep(1)
+        print('Valor guardado com sucesso!')
+    else:
+        print('Processando...')
+        sleep(1)
+        print('Valor duplicado![NÃO SALVO!]')
+    r = str(input('Deseja continuar? [S/N]')).upper().strip()
+    if r == 'N':
+        break
+print('=' * 45)
