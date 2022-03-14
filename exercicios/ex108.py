7625401f39dff68dfa58f4da3280d9c4fa61ad70
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-14 06:16:43 -0300

    ex108

--- exercicios/ex108.py
+++ exercicios/ex108.py
@@ -5,7 +5,7 @@ que consiga mostrar os números como um valor monetário formatado.
 import moedas
 
 p = float(input('Dígite um valor:R$  '))
-print(f'A metade de {p} é {moedas.metade(p)}')
-print(f'O dobro de {p} é {moedas.dobro(p)}')
-print(f'Aumentando o preço em 25% temos {moedas.aumentar(p, 25)}')
-print(f'Diminuindo o preço em 25% temos {moedas.diminuir(p, 25)}')
+print(f'A metade de {moedas.moeda(p)} é {moedas.moeda(moedas.metade(p))}')
+print(f'O dobro de {moedas.moeda(p)} é {moedas.moeda(moedas.dobro(p))}')
+print(f'Aumentando o preço de {moedas.moeda(p)} em 25% temos {moedas.moeda(moedas.aumentar(p, 25))}')
+print(f'Diminuindo o preço de {moedas.moeda(p)} em 25% temos {moedas.moeda(moedas.diminuir(p, 25))}')
