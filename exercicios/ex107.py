cc34a227f2b0e078fccda0126c2c8353959b6217
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-14 06:03:14 -0300

    edit

--- exercicios/ex107.py
+++ exercicios/ex107.py
@@ -4,5 +4,8 @@ Faça também um programa que importe esse módulo e use algumas dessas funçõe
 '''
 import moedas
 
-p = float(input('Dígite um valor: '))
-print(f'A metade de {p} é {moedas.metade()}')
+p = float(input('Dígite um valor:R$  '))
+print(f'A metade de {p} é {moedas.metade(p)}')
+print(f'O dobro de {p} é {moedas.dobro(p)}')
+print(f'Aumentando o preço em 25% temos {moedas.aumentar(p, 25)}')
+print(f'Diminuindo o preço em 25% temos {moedas.diminuir(p, 25)}')
