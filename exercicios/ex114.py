2bd12f9c2c0ae014d8b9bf3a5d89b623612872dc
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-14 15:02:43 -0300

    Update ex114.py

--- exercicios/ex114.py
+++ exercicios/ex114.py
@@ -1,3 +1,10 @@
 '''
 Crie um código em Python que teste se o site pudim está acessível pelo computador usado.
 '''
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except:
+    print('\033[;31;0mDeu ruin! \!-!/')
