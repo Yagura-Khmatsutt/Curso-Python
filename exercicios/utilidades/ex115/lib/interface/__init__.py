84280d094de1c5bc01d4c9159fe08e3e58486b3c
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-14 15:45:34 -0300

    main, lib, defs

---
+++ exercicios/utilidades/ex115/lib/interface/__init__.py
@@ -0,0 +1,33 @@
+def linha(tam=42):
+    return '-' * tam
+
+
+def leiaInt(msg):
+    while True:
+        try:
+            opc = str(input(msg))
+        except (ValueError, TypeError, InterruptedError):
+            print('\033[0;31mErro, dígite novamente...\033[m')
+            continue
+        except KeyboardInterrupt:
+            print('\033[31mErro, o usuario cancelou a operação!\033[m')
+            return 0
+        else:
+            return opc
+
+
+def head(txt):
+    print(linha())
+    print(txt.center(42))
+    print(linha())
+
+
+def menu(lista):
+    head('MENU PRINCIPAL')
+    i = 1
+    for item in lista:
+        print(f'{i} >> {item}')
+        i += 1
+    print(linha())
+    opção = leiaInt('Sua opção é: ')
+    return opção
---
+++ exercicios/utilidades/ex115/main.py
@@ -0,0 +1,5 @@
+from lib.interface import *
+
+head('CADASTROS v1.0')
+
+menu(['Novo cadastro', 'Verificar lista', 'Sair'])
