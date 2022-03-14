20513e57c39017a2d18f97012140451359e48ba5
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-14 16:01:58 -0300

    edit001

--- exercicios/utilidades/ex115/main.py
+++ exercicios/utilidades/ex115/main.py
@@ -2,4 +2,15 @@ from lib.interface import *
 
 head('CADASTROS v1.0')
 
-menu(['Novo cadastro', 'Verificar lista', 'Sair'])
+while True:
+    resp = menu(['Novo cadastro', 'Verificar lista', 'Sair'])
+    if resp == 1:
+        print('Opção 1')
+    elif resp == 2:
+        print('Opção 2')
+    elif resp == 3:
+        print('Encerrando sistema... volte sempre!')
+        break
+    else:
+        print('Erro!')
+
