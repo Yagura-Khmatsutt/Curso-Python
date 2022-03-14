7120a922e25cb41044077c62c4e0be548f47667f
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-14 06:55:19 -0300

    JO KEN PO

--- exercicios/ex110.py
+++ exercicios/ex110.py
@@ -2,7 +2,7 @@
  Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(),
  que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
 '''
-import moedas
+from moedas import ex110
 
 p = float(input('Dígite um valor:R$  '))
-moedas.resumo(p, 15, 15)
+ex110.resumo(p, 15, 15)
