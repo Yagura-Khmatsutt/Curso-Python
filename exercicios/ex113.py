52729ec5ab1214685a051c50ccce7673c6d84be3
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-14 14:55:55 -0300

    Update ex113.py

--- exercicios/ex113.py
+++ exercicios/ex113.py
@@ -2,20 +2,36 @@
 Reescreva a função leiaInt() que fizemos no desafio 104, incluindo agora a possibilidade da digitação de um
 número de tipo inválido. Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.
 '''
+
+
 def leiaInt(msg):
-    itsOk = False
-    valor = 0
+
     while True:
-        n = str(input(msg))
-        if n.isnumeric():
-            valor = int(n)
-            itsOk = True
-        else:
+        try:
+            i = str(input(msg))
+        except (ValueError, TypeError):
             print('\033[0;31mErro, dígite novamente...\033[m')
-        if itsOk:
-            break
-    return valor
+            continue
+        except (KeyboardInterrupt):
+            print('\033[0;31mUsuario cancelou a operação!\033[m')
+        else:
+            return i
 
 
-n = leiaInt('Dígite um número inteiro: ')
-print(f'O número digitado é {n}')
+def leiaFloat(msg):
+    while True:
+        try:
+            n = str(input(msg))
+        except (ValueError, TypeError):
+            print('\033[0;31mErro, dígite novamente...\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\033[0;31mUsuario cancelou a operação!\033[m')
+        else:
+            return n
+
+
+#MAIN
+i = leiaInt('Dígite um número inteiro: ')
+n = leiaFloat('Dígite um número real: ')
+print(f'O número digitado inteiro é {i} e o real é {n} ')
