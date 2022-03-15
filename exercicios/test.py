3285a6fc02659adb8213aac896134574ccda74f7
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-15 08:41:34 -0300

    Create test.py

---
+++ exercicios/test.py
@@ -0,0 +1,11 @@
+num = [3, 6, 4, 1]
+for n1, n2 in enumerate(num):
+    print(n1 + n2)
+letras = ('J', 'X', 'M', 'O', 'A', 'K')
+print(letras.index('A'))
+
+num = [2, 8, 4, 7]
+num.pop()
+num.insert(1, 3)
+num.append(6)
+print(num)
